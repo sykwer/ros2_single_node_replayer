e3743d3740f19bf6cbcb46e2dbcc0cb30a207ab5
veqcc <ryuta.kambe@tier4.jp>
2022-09-28 15:22:31 +0900

    topic remapping

--- recorder.py
+++ recorder.py
@@ -1,6 +1,7 @@
 import rclpy
 import os
 import argparse
+import yaml
 from datetime import datetime
 
 
@@ -26,11 +27,16 @@ def prepare_output_dir(params_file_name):
     os.chdir(logdir)
 
 
-def main(node_name, ns, package_name, executable_name):
+def main(node_name, ns, package_name, executable_name, remapping_file):
     rclpy.init()
     node = rclpy.create_node("node_input_topic_recorder")
     # names = node.get_node_names()
     info = node.get_subscriber_names_and_types_by_node(node_name, ns)
+
+    remappings = {}
+    remapping_file_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), remapping_file)
+    with open(remapping_file_path, 'r') as f:
+        remappings = yaml.safe_load(f)
 
     params_file_name = get_params_file_name(ns, node_name)
     prepare_output_dir(params_file_name)
@@ -44,9 +50,14 @@ def main(node_name, ns, package_name, executable_name):
     os.system(record_command)
 
     with open("ros2_run_" + package_name + "_" + executable_name, "w") as f:
-        f.write("ros2 run " + package_name + " " + executable_name +
-                " --ros-args --params-file " + params_file_name +
-                " -r __ns:=" + ns + " -r __node:=" + node_name)
+        exec_command = "ros2 run " + package_name + " " + executable_name + \
+                       " --ros-args --params-file " + params_file_name + \
+                       " -r __ns:=" + ns + " -r __node:=" + node_name
+
+        for k, v in remappings.items():
+            exec_command = exec_command + " -r " + k + ":=" + v
+
+        f.write(exec_command)
 
     node.destroy_node()
     rclpy.shutdown()
@@ -57,7 +68,7 @@ if __name__ == "__main__":
     parser.add_argument("executable", type=str, help="executable name")
     parser.add_argument("ns", type=str, help="namespace which target node belongs to")
     parser.add_argument("node", type=str, help="target node name")
+    parser.add_argument("remapping", type=str, help="topic remapping yaml file")
     args = parser.parse_args()
 
-    main(args.node, args.ns, args.package, args.executable)
-
+    main(args.node, args.ns, args.package, args.executable, args.remapping)
